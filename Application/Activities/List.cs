ba2f830f9721e55bbab8f853537234dd419b4c5d
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2021-11-27 20:50:24 +0200

    Added ExceptionMiddleware based on FluentValidation package in BackEnd

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,9 +1,9 @@
-﻿using Domain;
+﻿using Application.Core;
+using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Persistence;
-using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,35 +12,23 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>> { }
+        public class Query : IRequest<Result<List<Activity>>> { }
 
-        public class Handler : IRequestHandler<Query, List<Activity>>
+        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
         {
             private readonly ILogger<List> _logger;
             private readonly DataContext _context;
-            
+
             public Handler(DataContext context, ILogger<List> logger)
             {
                 _logger = logger;
                 _context = context;
             }
 
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                //try
-                //{
-                //    for (int i = 0; i < 10; i++)
-                //    {
-                //        cancellationToken.ThrowIfCancellationRequested();
-                //        await Task.Delay(1000, cancellationToken);
-                //        _logger.LogInformation($"Task {i} has completed");
-                //    }
-                //}
-                //catch (Exception ex) when (ex is TaskCanceledException)
-                //{
-                //    _logger.LogInformation("Task was canceled");
-                //}
-                return await _context.Activities.ToListAsync();
+                var result = await _context.Activities.ToListAsync();
+                return Result<List<Activity>>.Success(result);
             }
         }
     }
