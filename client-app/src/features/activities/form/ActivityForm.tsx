ef062c85a51871194618e854f6d8507d0e6dbe64
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2022-02-06 21:55:46 +0200

    FronEnd || Updated logic in activities features

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -12,7 +12,7 @@ import ReusableTextArea from '../../../app/common/form/ReusableTextArea';
 import { categoryOptions } from '../../../app/common/options/categoryOptions';
 import ReusableSelectInput from '../../../app/common/form/ReusableSelectInput';
 import ReusableDateInput from '../../../app/common/form/ReusableDateInput';
-import { Activity } from '../../../app/models/activity';
+import { Activity, ActivityFormValues } from '../../../app/models/activity';
 import { v4 as uuid } from 'uuid';
 
 export default observer (function ActivityForm() {
@@ -21,16 +21,8 @@ export default observer (function ActivityForm() {
     const {loading, loadingInitial, loadActivity, createActivity, updateActivity } = activityStore;
     const {id} = useParams<{id: string}>();
 
-    const [activity, setActivity] = useState<Activity>({
-        id: '',
-        title: '',
-        description: '',
-        category: '',
-        date: null,
-        city: '',
-        venue: ''
-    });
-
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
+    
     const validationSchema = Yup.object({
         title: Yup.string().required('The activity title is required'),
         description: Yup.string().required('The description title is required'),
@@ -41,11 +33,11 @@ export default observer (function ActivityForm() {
     });
 
     useEffect(() =>{
-        if(id) loadActivity(id).then(activity => setActivity(activity!));
+        if(id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)));
     }, [id, loadActivity])
 
-    function handleFormSubmit(activity: Activity) {
-        if (activity.id.length === 0) {
+    function handleFormSubmit(activity: ActivityFormValues) {
+        if (!activity.id) {
             let newActivty = {
                 ...activity,
                 id: uuid()
@@ -83,7 +75,7 @@ export default observer (function ActivityForm() {
                         <ReusableTextInput placeholder='Venue' name='venue' />
                         <Button 
                             disabled={isSubmitting || !dirty || !isValid}
-                            loading={loading} floated='right' 
+                            loading={isSubmitting} floated='right' 
                             positive type='submit' content='Submit' />
                         <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
                     </Form>
