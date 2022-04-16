af094a97cdf3ac7d4380413bc17c43f53c6e5bf1
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2022-04-16 14:00:13 +0300

    BackEnd || Fix for image souces in CSP

--- API/Startup.cs
+++ API/Startup.cs
@@ -45,7 +45,7 @@ namespace API
             app.UseReferrerPolicy(opt => opt.NoReferrer());
             app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
             app.UseXfo(opt => opt.Deny());
-            app.UseCspReportOnly(opt => opt
+            app.UseCsp(opt => opt
                 .BlockAllMixedContent()
                 .StyleSources(s => s.Self().CustomSources(
                     "https://fonts.googleapis.com",
@@ -57,6 +57,7 @@ namespace API
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
                 .ImageSources(s => s.Self().CustomSources(
+                    "blob:",
                     "https://res.cloudinary.com",
                     "https://www.facebook.com",
                     "https://platform-lookaside.fbsbx.com",
